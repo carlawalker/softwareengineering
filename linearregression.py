762b3d567309d896098be348c7af80df14be3bac
joaopamatias <joaopamatias@gmail.com>
2017-01-17 11:52:14 +0000

    linearregression.py | datamanagement module integration
    Instead of copying the code, I imported the dataset from the
    datamanagement.py

--- linearregression.py
+++ linearregression.py
@@ -6,40 +6,17 @@ Created on Tue Jan 10 11:55:46 2017
 """
 
 
-# Import pandas, glob, sklearn, matplotlib and numpy with the usual conventions
-# pandas is for easy to use data structures and data analysis
-# glob is for the data import 
+# Import sklearn, matplotlib and numpy with the usual conventions
 # sklearn and matplotlib are important for our regression and the ploting 
 # numpy is required for the calculation of the coefficents 
-import pandas as pd
-import glob
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 import numpy as np
 
-# set directory of data import files
-# @var curr = files directory
-curr = "./Desktop/Software Engineering/python/clone_repository/softwareengineering/Data/RV*.xlsx"
 
-#Using glob to create a list with all the Research Variables (RV) files in the Data folder.  
-glob.glob(curr)
+# Import dataset from the Datamanagement Module
+from datamanagement import dataset
 
-#Creation of the data frame. This technique appends all the different rows according to the same year in column.  
-dataset = pd.DataFrame()
-
-
-# dataset import function
-# @param f = current file in directory
-for f in glob.glob(curr):
-    df = pd.read_excel(f)
-    dataset = dataset.append(df, ignore_index=True)
-    
-#Transposition of the resulting table. Year (common row) is used as index while transposed. This way we obtain the end result as described above.
-dataset = dataset.set_index("year").T
-
-#Option to save the table as csv for visualization
-dataset.to_csv("final_dataset.csv")
-print (dataset)
 
 # Plot the two variables against each other, x remains the same, y can be changed  
 # @var x_df = DataFrame column for base "exchange_rate"
@@ -47,14 +24,17 @@ print (dataset)
 x_df = dataset.loc[:,["exchange_rate"]]
 y_df = dataset.loc[:,["swiss_travels"]]
 
+
 # Create linear regression object
 regr = linear_model.LinearRegression()
 regr.fit(x_df, y_df)
+
 
 # Plot outputs and show the graph 
 plt.scatter(x_df, y_df, color='black')
 plt.plot(x_df, regr.predict(x_df), color='blue', linewidth=3)
 plt.show()
+
 
 # Calculate coefficients
 print('Coefficients: \n', regr.coef_)
